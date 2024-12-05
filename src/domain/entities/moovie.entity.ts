4f7cbaf1cb8f83d17694b1f243c2b79eef791f3b
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-12-05 18:13:18 -0300

    adicionar tipagem do movie

--- src/domain/entities/moovie.entity.ts
+++ src/domain/entities/moovie.entity.ts
@@ -7,11 +7,20 @@ export interface IMovie {
     hasCreditCookie: boolean;
 }
 
-export const movieSchema = new mongoose.Schema<IMovie>({
+export interface IMovieDoc extends IMovie, mongoose.Document {}
+
+export interface IMovieModel extends mongoose.Model<IMovieDoc> {}
+
+const MovieSchemaFields: mongoose.SchemaDefinition<IMovie> = {
     title: { type: String },
     rating: String,
     releaseYear: Number,
     hasCreditCookie: Boolean,
-});
+};
 
-export const Movie = mongoose.model<IMovie>("Movie", movieSchema);
+export const movieSchema = new mongoose.Schema<IMovieDoc>(MovieSchemaFields);
+
+export const Movie = mongoose.model<IMovieDoc, IMovieModel>(
+    "Movie",
+    movieSchema
+);
